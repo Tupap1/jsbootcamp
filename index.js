9d5f5e9db76a2e1eaef47d1df001742a18258c7e
Tupap1 <126369055+Tupap1@users.noreply.github.com>
2023-11-01 23:30:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+   let contar1 = 0  
+
+   function asd() {
+      console.log("agregaste 1")
+      contar1 = contar1 + 1
+      //Contador.InnerText = contar
+      console.log(contar1) 
+   }
+
+   asd()
+   asd()
+   asd()
+
+   //let Contador = document.getElementById("Contador")
+
